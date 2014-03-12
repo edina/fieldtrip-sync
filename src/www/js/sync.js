8de9f463f8de426fd17398b2a32e27dbe7a1f725
George Hamilton <george.hamilton@ed.ac.uk>
2014-03-12 12:58:44 +0000

    Issue #1 add settings to deps

--- src/www/js/sync.js
+++ src/www/js/sync.js
@@ -1,6 +1,9 @@
-define(['records', 'config', './login'], function(records, config, login){
+define(['records', 'settings', 'config', './login'], function(records, settings, config, login){
     var cloudProviderUrl = config["pcapi_url"]+"/" + config["pcapi_version"] + "/pcapi";
     login.init(cloudProviderUrl);
+
+    //settings.addControl('pcapi-url', 'select');
+    settings.get('pcapi-url');
 
     $(document).on('vclick', '#home-content-login', $.proxy(login.loginCloud, login));
 });
