fbcec5ac9ab5e159b27c17d7ece82f92ff00b690
George Hamilton <george.hamilton@ed.ac.uk>
2014-05-07 10:50:13 +0100

    Issue edina/fieldtrip-open#31 remove dummy test

--- src/www/js/tests.js
+++ src/www/js/tests.js
@@ -1,9 +1,7 @@
 "use strict";
 define(['./upload'], function(upload) {
     var run = function() {
-        test('Sync: do something.', function() {
-            equal(2, 2, 'The return should be 2.');
-        });
+        module("Sync");
     };
     return {run: run}
 });
