51e7e19085ab8522bdb9d51f8a67823e725c6fcb
George Hamilton <george.hamilton@ed.ac.uk>
2014-06-16 12:00:21 +0100

    Issue edina/fieldtrip-gb#8 get sync sys test working

--- src/www/js/tests.js
+++ src/www/js/tests.js
@@ -31,7 +31,7 @@ DAMAGE.
 
 "use strict";
 
-/* global asyncTest */
+/* global asyncTest, ok */
 
 define(['utils', './login', './upload', 'tests/systests'], function(// jshint ignore:line
     utils, login, upload, sts) {
@@ -45,9 +45,10 @@ unit: {
 },
 sys:{
     run: function(){
+        var user = utils.getCloudTestUser();
         var loginCloud = function(cb){
-            var allowStr = 'document.querySelector(\'input[name="allow_access"]\').click();';
-            var loginStr = 'if(document.getElementById("login_email")){document.getElementById("login_email").value="george.hamilton@ed.ac.uk";document.getElementById("login_password").value="un5afe";document.querySelector(\'input[type="submit"]\').click();}else{}'; // jshint ignore:line
+            var allowStr = 'document.querySelector(\'button[name="allow_access"]\').click();';
+            var loginStr = 'if(document.getElementsByName("login_email").length > 0){document.getElementsByName("login_email")[1].value="' + user.name + '";document.getElementsByName("login_password")[1].value="' + user.pass + '";document.querySelector(\'button[type="submit"]\').click();}else{}'; // jshint ignore:line
 
             login.loginCloud(
                 function(){
@@ -55,9 +56,7 @@ sys:{
                     cb();
                 },
                 function(cbrowser){
-                    console.log("=>");
                     setTimeout(function(){
-                        console.log(cbrowser);
                         cbrowser.executeScript(
                             {
                                 code: loginStr
@@ -79,52 +78,48 @@ sys:{
         };
 
         module("Sync");
-        console.log("Do sync " + utils.isMobileDevice());
 
         if(utils.isMobileDevice()){
             asyncTest("Test Sync", function(){
+                // ensure used is logged out
+                login.logoutCloud();
 
                 loginCloud(function(){
-                    console.log("logged in");
-                // add new text record
-                // sts.addRecord('test sync', function(newCount){
-                //     console.log("add record");
-                //     sts.goToRecordsPage(function(){
-                //         // click on sync button
-                //         $('#saved-annotations-page-header-login-sync').mousedown();
+                    // add new text record
+                    sts.addRecord('test sync', function(newCount){
+                        sts.goToRecordsPage(function(){
+                            // click on sync button
+                            $('#saved-records-page-header-login-sync').mousedown();
 
-                //         intervalTest({
-                //             'id': '#saved-annotations-page-header-login-sync',
-                //             'poll': 1000,
-                //             'delay': 1000,
-                //             'test': function(){
-                //                 return ($('#saved-annotation-sync-popup div').length > 0);
-                //             },
-                //             'cb': function(success){
-                //                 ok(success, 'start sync');
-                //                 //$('a.sync-confirm').click();
-                //                 $('a.sync-confirm').mousedown();
+                            sts.intervalTest({
+                                'id': '#saved-records-page-header-login-sync',
+                                'poll': 1000,
+                                'delay': 1000,
+                                'test': function(){
+                                    return ($('#saved-records-sync-popup div').length > 0);
+                                },
+                                'cb': function(success){
+                                    ok(success, 'start sync');
+                                    $('a.sync-confirm').mousedown();
 
-                //                 // force close of popup
-                //                 //$('#saved-annotation-sync-popup').popup('close');
-
-                //                 intervalTest({
-                //                     'id': 'sync-confirm',
-                //                     'test': function(){
-                //                         // sync is complete when cursor is saved
-                //                         return that.storage.getCloudLogin().cursor !== undefined;
-                //                     },
-                //                     'cb': function(success){
-                //                         ok(success, 'sync complete');
-                //                         start();
-                //                     },
-                //                     'attempts': 1000,
-                //                     'poll': 1000
-                //                 })
-                //             }
-                //         });
-                //     })
-                //});
+                                    sts.intervalTest({
+                                        'id': 'sync-confirm',
+                                        'test': function(){
+                                            // sync is complete when cursor is saved
+                                            return login.getUser().cursor !== undefined;
+                                        },
+                                        'cb': function(success){
+                                            ok(success, 'sync complete');
+                                            //start();
+                                            sts.complete();
+                                        },
+                                        'attempts': 1000,
+                                        'poll': 1000
+                                    });
+                                }
+                            });
+                        });
+                    });
                 });
             });
         }
